863c3d00ef77dd6ef37110f443445a245f0ba59a
pglj2 <pglj2@cin.ufpe.br>
2018-04-11 20:50:49 -0300

    finalizada questão 2, porem ainda pode-se melhorar

--- lista2/problema2.js
+++ lista2/problema2.js
@@ -1,7 +1,8 @@
-
-var margin = {top: 20, right: 20, bottom: 30, left: 40};
-var width = 400 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+var margin = {top:40,left:40,bottom:20,right:20};
+var totalWidth = 400;
+var totalHeight = 400;
+var width = totalWidth - margin.left - margin.right;
+var height = totalHeight - margin.top - margin.bottom;
 
 var x = d3.scaleBand()
       .range([0, width])
@@ -61,6 +62,15 @@ d3.selectAll("rect").attr("stroke","black").attr("stroke-width",3);
       .call(d3.axisLeft(y));
 
 
+//Scatter
+
+var margin2 = {top:40,left:80,bottom:80,right:20};
+var totalWidth2 = 800;
+var totalHeight2 = 400;
+var width2 = totalWidth2 - margin2.left - margin2.right;
+var height2 = totalHeight2 - margin2.top - margin2.bottom;
+
+
 function transformAllDateStart(){
     var t = [new Date(parseInt(trips[0].start.substring(6,10)), parseInt(trips[0].start.substring(3,5) - 1),
             parseInt(trips[0].start.substring(0,2)))];
@@ -105,77 +115,74 @@ function getPrice(){
 
 var price = getPrice();
 
+var scat;
 function scaterGen(){
-    var scater = [{"days":days[0] , "price":price[0]}];
+    this.scat = [{"days":days[0] , "price":price[0]}];
     for(var i = 1; i < days.length; i++){
-        scater.push({"days":days[i], "price":price[i]});
+        scat.push({"days":days[i], "price":price[i]});
     }
 
-    return scater;
+    //return scat;
 }
 
-var scat = scaterGen();
+scaterGen();
 
 var svg2 = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width2 + margin2.left + margin2.right)
+      .attr("height", height2 + margin2.top + margin2.bottom)
       .append("g")
       .attr("transform", 
-            "translate(" + margin.left + "," + margin.top + ")");
-      
-
-margin = {left: 80, right:20, top:50, bottom: 100};
-width = 400 - margin.left - margin.right;
-heigth = 400 - margin.top - margin.bottom;
-
-var t = d3.transition().duration(750);
+            "translate(" + margin2.left + "," + margin2.top + ")");
 
 var xAxisGroup = svg2.append("g")
                     .attr("class", "x axis")
-                    .attr("transform", "translate(0,"+height+")");
+                    .attr("transform", "translate(0,"+height2+")");
 
 var yAxisGroup = svg2.append("g")
                     .attr("class", "y axis");
 
-var x2 = d3.scaleLinear()
-            .range([0,width]);
+var xScale = d3.scaleLinear()
+            .range([0,width2]);
 
-var y2 = d3.scaleLinear()
-        .range([height,0]);
+var yScale = d3.scaleLinear()
+        .range([height2,0]);
 
 svg2.append("text")
-    .attr("y", height + 50)
-    .attr("x", width / 2)
+    .attr("y", height2 + 50)
+    .attr("x", width2 / 2)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("Month");
+    .text("Days");
 
 var yLabel = svg2.append("text")
             .attr("y", -60)
-            .attr("x", -(height / 2))
+            .attr("x", -(height2 / 2))
             .attr("font-size", "20px")
             .attr("text-anchor", "middle")
             .attr("transform", "rotate(-90)")
-            .text("Revenue");
+            .text("Price");
 
 
-x2.domain([0, d3.max(days)]);
-y2.domain([0, d3.max(price)]);
+xScale.domain([0, d3.max(days)])
+yScale.domain([0, d3.max(price)]).range([height2,0]);
 
-var xAxisCall = d3.axisBottom(x2);
-xAxisGroup.transition(t).call(xAxisCall);
+var xAxisCall = d3.axisBottom(xScale);
+xAxisGroup.call(xAxisCall);
 
-var yAxisCall = d3.axisLeft(y2);
-yAxisGroup.transition(t).call(yAxisCall);
+var yAxisCall = d3.axisLeft(yScale);
+yAxisGroup.call(yAxisCall);
 
 var circles = svg2.selectAll("circle")
-                .data(days)
+                          .data(scat)
+                          .enter()
+                          .append("g");
 
-circles.enter().append("circle")
-                .attr("fill", "grey")
-                .attr("cy", y(0))
-                .attr("cx", function(d){ return x(d.days) + x.bandwidth() / 2})
-                .attr("r", 5)
-
-
+circles.append("circle")
+                       .attr("cx", function (d) { return d.price; })
+                       .attr("cy", function (d) {  
+                        return d.days; })
+                       
+                      .attr("r", 3)
+                      .attr("fill", "green")
+                      
 
